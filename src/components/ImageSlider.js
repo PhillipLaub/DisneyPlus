c9380373c85c65fa9952a223f93f0376ca85aa41
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2021-05-19 12:28:26 -0700

    viewers completed

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -10,6 +10,7 @@ function ImageSlider() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+    autoplaySpeed: 4000,
   };
 
   return (
@@ -18,7 +19,13 @@ function ImageSlider() {
         <img src="/images/slider-badging.jpg" alt="" />
       </Wrap>
       <Wrap>
-        <img src="/images/slider-badging.jpg" alt="" />
+        <img src="/images/slider-badag.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scale.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scales.jpg" alt="" />
       </Wrap>
     </Carousel>
   );
@@ -26,6 +33,43 @@ function ImageSlider() {
 
 export default ImageSlider;
 
-const Carousel = styled(Slider)``;
+const Carousel = styled(Slider)`
+  padding-top: 20px;
 
-const Wrap = styled.div``;
+  .slick-list {
+    overflow: visible;
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button::before {
+    color: white;
+  }
+
+  button {
+    z-index: 1;
+  }
+`;
+
+const Wrap = styled.div`
+  cursor: pointer;
+  img {
+    border: 4px solid transparent;
+    width: 100%;
+    height: 100%;
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+
+    transition-duration: 300ms;
+
+    &:hover {
+      border: 4px solid rgba(249, 249, 249, 0.8);
+    }
+  }
+`;
