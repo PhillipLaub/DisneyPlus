f5e7e6b5d7a72de43875c79686ff778a776cbf94
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2021-05-19 10:27:07 -0700

    imported assets, working on header

---
+++ src/components/Header.js
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a href="#">
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+        <a href="#">
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a href="#">
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="#">
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="#">
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+        <a href="#">
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
+}
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    text-decoration: none;
+    color: white;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+    }
+  }
+`;
