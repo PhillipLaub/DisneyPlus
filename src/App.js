fa74f2a79e79c8556c050ed84878bf0fb6fd9ba9
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2021-05-20 09:28:12 -0700

    Login page complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Detail from "./components/Detail";
+import Login from "./components/Login";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -14,6 +15,9 @@ function App() {
         <Switch>
           <Route path="/detail">
             <Detail />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
           <Route exact path="/">
             <Home />
